92338726d94da055a3de3b8edd9b69ba52881951
Roy <roy154638@gmail.com>
2024-12-12 15:40:10 +0800

    Add User Base

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False)  # 字串類型，限制長度為50，必填，且必須唯一
+    password = Column(String(255), nullable=False)  # 字串類型，設為255長度以容納加密密碼，必填
+    email = Column(String(255), unique=True, nullable=False)  # 字串類型，設為255長度，必填，且必須唯一
