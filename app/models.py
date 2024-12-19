f21d7c35baca747e81e53bfcfefaf32cbbf45ac6
Roy <roy154638@gmail.com>
2024-12-19 15:18:24 +0800

    add status column

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Date, Integer, String, Boolean
+from sqlalchemy import Column, Date, Integer, String, Boolean, Enum
 from .database import Base
+import enum
 
 # Define Model
 class Todo(Base):
@@ -10,6 +11,11 @@ class Todo(Base):
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
 
+class UserStatus(enum.Enum):
+    ACTIVE = "active"
+    INACTIVE = "inactive"
+    DELETED = "deleted"
+
 class User(Base):
     __tablename__ = "users"
 
@@ -17,3 +23,4 @@ class User(Base):
     username = Column(String(50), unique=True, nullable=False)  # 字串類型，限制長度為50，必填，且必須唯一
     password = Column(String(255), nullable=False)  # 字串類型，設為255長度以容納加密密碼，必填
     email = Column(String(255), unique=True, nullable=False)  # 字串類型，設為255長度，必填，且必須唯一
+    status = Column(Enum(UserStatus), default=UserStatus.ACTIVE, nullable=False)  # 使用Enum定義狀態，預設值為ACTIVE
